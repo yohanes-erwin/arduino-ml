34bc9ff2e498c86ad9352e796ae563e1b3793a66
Yohanes Erwin <erwin.setiawan789@gmail.com>
2020-02-23 09:45:50 +0700

    Added DHT11 example

---
+++ esp32_examples/esp32-devkit-arduino-dht11/esp32-devkit-arduino-dht11.ino
@@ -0,0 +1,39 @@
+// Author: Erwin Ouyang, aiotedge.tech
+// Date  : 23 Feb 2020
+
+#include <DHT.h>
+
+#define DHTPIN  4
+#define DHTTYPE DHT11
+
+// Setup DHT pin and type
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() 
+{
+  // Initialize serial port
+  Serial.begin(115200);
+  // Initialize DHT sensor
+  dht.begin();
+}
+
+void loop()
+{ 
+  // Read temperature
+  float temp = dht.readTemperature();
+  // Read humidity
+  float hum = dht.readHumidity();
+  
+  // Print temperature 
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print("°C ");
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.println("%");
+
+  // Maximum sampling rate of DHT11 is 1Hz
+  // So, the minimum delay is one second 
+  delay(1000);
+}
+
