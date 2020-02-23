a019733090655d64fbaee89ebdecaeb138a87950
Yohanes Erwin <erwin.setiawan789@gmail.com>
2020-02-23 09:58:48 +0700

    Updated bike sharing model

--- 1_simple_linear_regression/2_c_model/bike-sharing-c-arduino-esp32/bike-sharing-c-arduino-esp32.ino
+++ 1_simple_linear_regression/2_c_model/bike-sharing-c-arduino-esp32/bike-sharing-c-arduino-esp32.ino
@@ -1,6 +1,13 @@
 // Author: Erwin Ouyang, aiotedge.tech
 // Date  : 22 Feb 2020
 
+// Train set
+int x_train[80] = 
+int y_train[80] = {0};
+// Test set
+int x_test[20] = {0};
+int y_test[20] = {0};
+
 void setup()
 {
   // Serial port for debugging purposes
@@ -10,5 +17,10 @@ void setup()
 void loop()
 {
   
+}
+
+void create_data_set()
+{
+
 }
 
